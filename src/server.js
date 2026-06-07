59a60aca6caa499351df508ff7893ec2850b34a4
Gabi Hernandez <canelonmariagabriela3@gmail.com>
2026-06-06 23:51:00 -0300

    Removido servidor duplicado e ajustado caminhos estaticos

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
-require('dotenv').config({ path: '../.env' }); // O .env costuma ficar na raiz, fora da src
-const express = require('express');
-const db = require('./Config/index'); // Importa a conexão do MySQL
-const clienteRoutes = require('./routes/clienteRoutes'); // Importa as rotas do cliente
-const historicoRoutes = require('./routes/historicoRoutes'); // Importa as rotas do histórico
-const app = express();
-const PORT = process.env.PORT || 3000;
-const cors = require('cors');
-
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public')); // Serve arquivos estáticos da pasta 'public'
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', clienteRoutes); // Prefixa as rotas com /api, por exemplo: /api/clientes
 app.use('/api', historicoRoutes); // Prefixa as rotas com /api, por exemplo: /api/historico
+
+// ROTA CORINGA: Garante que o Render entregue o index.html na tela do celular
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 // O teste de conexão que você queria
 db.getConnection()
     .then(conn => {
