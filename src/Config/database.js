74e66aa282a11100979c4b6f0b1b12ed73259b50
Gabi Hernandez <canelonmariagabriela3@gmail.com>
2026-06-06 23:27:48 -0300

    Ajuste na conexão do banco para o Render

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,13 +1,28 @@
 //mapa para as variaveis de ambiente do banco de dados 
 require('dotenv').config(); // Garante que o Node leia o .env
 
-module.exports = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'Controle_Vendas',
-   
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0
-};
+let config;
+
+// Se o Render enviar a URL cheia do banco, nós usamos ela
+if (process.env.DATABASE_URL) {
+    config = {
+        uri: process.env.DATABASE_URL,
+        // Isso resolve erros de conexão segura exigidos pela nuvem
+        ssl: { rejectUnauthorized: false } 
+    };
+} else {
+    // Se não tiver a URL (no seu computador local), usa a sua configuração padrão
+    config = {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'Controle_Vendas'
+    };
+}
+
+// Mantém os seus limites de conexões idênticos
+config.connectionLimit = 10;
+config.waitForConnections = true;
+config.queueLimit = 0;
+
+module.exports = config;
