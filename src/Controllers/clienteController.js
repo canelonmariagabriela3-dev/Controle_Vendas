fcaebd1d65c7a6cd032916669c7d3601300a5791
Gabi Hernandez <canelonmariagabriela3@gmail.com>
2026-06-10 17:46:38 -0300

    separa CSS em style.css, corrige sintaxe no controller

--- src/Controllers/clienteController.js
+++ src/Controllers/clienteController.js
@@ -1,11 +1,11 @@
-const Cliente= require('../Models/Cliente');
+const Cliente = require('../Models/Cliente');
 
 const ClienteController = {
     criar: async (req, res) => {
         try {
-            const {nome, telefone, local_trabalho} = req.body;
-            
-            const id = await Cliente.criar(nome, telefone, local_trabalho );
+            const { nome, telefone, local_trabalho } = req.body;
+
+            const id = await Cliente.criar(nome, telefone, local_trabalho);
             res.status(201).json({ id, message: 'Cliente criado com sucesso!' });
         }
         catch (err) {
@@ -21,23 +21,23 @@ const ClienteController = {
         catch (err) {
             console.error('Erro ao listar clientes:', err);
             res.status(500).json({ error: 'Erro ao listar clientes' });
-        }   
-        },
+        }
+    },
     buscarPorId: async (req, res) => {
         try {
-            const{ id } = req.params;
-            const cliente= await Cliente.buscarPorId(id);
+            const { id } = req.params;
+            const cliente = await Cliente.buscarPorId(id);
             if (!cliente) {
-                res.status(404);json({error:'Cliente não encontrado'});
-                return;
+                return res.status(404).json({ error: 'Cliente não encontrado' });
+
             } else {
-                    res.status(200).json(cliente);
-                }
-            } catch (err) {
-                console.error('Erro ao buscar cliente:', err);
-                res.status(500).json({ error: 'Erro ao buscar cliente' });
+                res.status(200).json(cliente);
             }
+        } catch (err) {
+            console.error('Erro ao buscar cliente:', err);
+            res.status(500).json({ error: 'Erro ao buscar cliente' });
         }
+    }
 };
 
 
